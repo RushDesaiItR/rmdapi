ffca2708b6d156eafde1c6da229da4014fee55c0
Rushikesh Desai <rushikeshmahavirdesaithree@gmail.com>
2021-11-03 08:58:27 +0530

    backu newlll

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json()); //or use body-parser middleware to parse the JSON body from HTTP request
-
+const port = process.env.PORT || 3800;
 // Import Routes
 const authRoute = require('./routes/index');
  
@@ -11,7 +11,7 @@ const authRoute = require('./routes/index');
 app.use('/api', authRoute);
 
 
-const port = 3000;
+
 app.listen(port, function(){
     console.log("Server running on localhost:" + port);
 });
