3d71104ccb569e9456208542f78df9c08932b2ad
Rushikesh Desai <rushikeshmahavirdesaithree@gmail.com>
2021-11-28 11:15:03 +0530

    first commit

--- serverexcel.js
+++ serverexcel.js
@@ -7,11 +7,13 @@ const app = express();
 app.use(express.json()); //or use body-parser middleware to parse the JSON body from HTTP request
 const port = process.env.PORT || 3800;
 app.post("/readfile",(req, res)=>{
-    console.log("dsd")
+   
     var workbook = XLSX.readFile(req.file)
     var sheet_name_list = workbook.SheetNames;
     res.send(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
-    console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
+})
+app.get("/",(req, res)=>{
+    res.send("sdsds")
 })
 function call(){
    
