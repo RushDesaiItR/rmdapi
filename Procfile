101a707adca166c4f4e4be75f4415705c0b94ff5
Rushikesh Desai <rushikeshmahavirdesaithree@gmail.com>
2021-11-28 11:12:02 +0530

    first commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:node index.js
+web:node serverexcel.js
---
+++ serverexcel.js
@@ -0,0 +1,25 @@
+
+
+const express = require('express');
+var XLSX = require('xlsx');
+const app = express();
+
+app.use(express.json()); //or use body-parser middleware to parse the JSON body from HTTP request
+const port = process.env.PORT || 3800;
+app.post("/readfile",(req, res)=>{
+    console.log("dsd")
+    var workbook = XLSX.readFile(req.file)
+    var sheet_name_list = workbook.SheetNames;
+    res.send(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
+    console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
+})
+function call(){
+   
+var workbook = XLSX.readFile('FINAL450.xlsx');
+var sheet_name_list = workbook.SheetNames;
+console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
+}
+
+app.listen(port, function(){
+    console.log("Server running on localhost:" + port);
+});
