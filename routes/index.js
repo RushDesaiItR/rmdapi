c3c45b8ee7301a871e41bc66b8fc094b57c6b9f5
Rushikesh Desai <rushikeshmahavirdesaithree@gmail.com>
2022-01-09 11:20:53 +0530

    post get

--- routes/index.js
+++ routes/index.js
@@ -21,8 +21,8 @@ router.get("/getpostsbyid/:id", userController.getUserPosts)
 
 router.get("/gethomedatabyid/:id",userController.getHomeAllStories)
 router.get("/getfriendsbyid/:id",userController.getFriendsById)
-// router.get("/gethomedatabyidtwo/:id",userController.getHomeAllPosts)
-// router.get("/gethomedatabyidthree/:id",userController.getHomeAllUserSuggestions)
+router.get("/gethomedatabyidtwo/:id",userController.getHomeAllPosts)
+router.get("/gethomedatabyidthree/:id",userController.getHomeAllUserSuggestions)
 
 
 
